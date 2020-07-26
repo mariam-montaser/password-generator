385441b6db98dc6b36749a6bc86c47509763110a
mariam-montaser <mariammontaser2018@gmail.com>
2020-07-26 16:33:10 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  length: number = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password: string = '';
+
+  onChangeLength(length: string) {
+    // this.length = +length;
+    const parsed = parseInt(length);
+    if (!isNaN(parsed)) {
+      this.length = parsed;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    this.password = this.generatePassword();
+  }
+
+  generatePassword(): string {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    let generatedPass = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPass += validChars[index];
+    }
+
+    return generatedPass;
+  }
 }
